f3cee3f1ebb4415642a439801c64bf63b9f28893
SeungJoonBaik <janghk012@gmail.com>
2023-10-04 13:03:31 +0900

    first

---
+++ src/main/java/com/green/codingtest/java/ExchangeAlphabet.java
@@ -0,0 +1,34 @@
+package com.green.codingtest.java;
+
+import java.util.Scanner;
+// 입력:ABCDEFghijklmnop
+// 출력:abcdefGHIJKLMNOP
+
+//해결방법.
+/*
+ 1.스트링 입력받음.
+2.string을 char을 각각 입력받음
+3.입력받은 char을 int로 변환
+4.변환된 char을 반복문을 통하여 분류
+5.즉시 출력.
+ */
+public class ExchangeAlphabet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // 꼭 외워두기. 스캐너.
+        String alpha=sc.next(); // 스트링 받을때까지 대기.
+        for(int i=0;i<alpha.length();i++) // alpha.length() 는 alpha 의 string 숫자를 받아 int로 변환.
+        {
+            int tmp=alpha.charAt(i);// tmp 변수 선언 및 string변수인 alpha를 charAt함수를 통해 각각 입력받음.
+            if(65<=tmp&&tmp<=90)//만약 tmp가 소문자면
+            {
+                tmp+=32; // tmp에 32를 더해줌(대문자로 변환[아스키 코드 상으로 +32 해줌.])
+                System.out.printf("%c",(char)tmp);//즉시출력(char 형변환)
+            }else if(97<=tmp&&tmp<=122)//만약 tmp가 대문자면
+            {
+                tmp-=32;//tmp에 32를 빼줌(소문자로 변환[아스키 코드 상으로 -32 해줌.])
+                System.out.printf("%c",(char)tmp);//즉시출력
+            }
+        }
+
+    }
+}
---
+++ src/main/java/com/green/codingtest/java/StringGlue.java
@@ -0,0 +1,31 @@
+package com.green.codingtest.java;
+
+import java.util.Scanner;
+//문제설명
+/*
+
+두 개의 문자열 str1, str2가 공백으로 구분되어 입력으로 주어집니다.
+입출력 예와 같이 str1과 str2을 이어서 출력하는 코드를 작성해 보세요.
+
+입력 #1
+apple pen
+출력 #1
+applepen
+입력 #2
+Hello World!
+출력 #2
+HelloWorld!
+ */
+
+//해결방법
+
+//애초에 스페이스바로 두개의 string이 구분지어지기 때문에 스페이스바는 어떤 string에도 입력되어지지 않는다.
+//따라서 a b string을 이어서 출력해주면 된다.
+public class StringGlue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.printf("%s%s",a,b);
+    }
+}
