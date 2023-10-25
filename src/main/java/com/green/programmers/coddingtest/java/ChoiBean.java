66de429ec63342164617801f59fb43cac4a57267
SeungJoonBaik <janghk012@gmail.com>
2023-10-25 19:12:59 +0900

    10월25일 공부

---
+++ src/main/java/com/green/programmers/coddingtest/java/ChoiBean.java
@@ -0,0 +1,13 @@
+package com.green.programmers.coddingtest.java;
+
+import java.util.*;
+
+public class ChoiBean {
+    class Solution {
+        public int solution(int[] array) {
+            int answer = 0;
+
+            return answer;
+        }
+    }
+}
